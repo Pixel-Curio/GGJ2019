95f463d1defb2c6f617d4f1ff3924cc6d131fc4d
Michael Sanders <mike.gamedev@gmail.com>
2019-01-27 14:28:02 -0800

    Setup basic colliders for all leters.

---
+++ Assets/_Project/Scripts/Arrow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace PixelCurio
+{
+    public class Arrow : MonoBehaviour
+    {
+        public void OnTriggerEnter2D(Collider2D col)
+        {
+            Debug.Log(col.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+        }
+    }
+}
